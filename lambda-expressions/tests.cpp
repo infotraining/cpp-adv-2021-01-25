fec6ae2d60456af874b91359c52885dea03fa305
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-01-25 12:57:51 +0100

    lambda - part 1

--- lambda-expressions/tests.cpp
+++ lambda-expressions/tests.cpp
@@ -116,6 +116,12 @@ public:
         ++counter_;
         std::cout << "Item: " << a << "\n";
     }
+
+    void operator()(const std::string& text) 
+    {
+        ++counter_;
+        std::cout << "String: " << text << "\n";
+    }
     
     int counter() const
     {
@@ -160,4 +166,204 @@ TEST_CASE("functors")
         Printer used_printer = std::for_each(std::begin(vec), std::end(vec), Printer{});
         std::cout << "Printer used " << used_printer.counter() << " times\n";
     }
+}
+
+namespace explain
+{
+    template <typename Iter, typename UnPred>
+    Iter find_if(Iter first, Iter last, UnPred pred)
+    {
+        for(Iter it = first; it != last; ++it)
+        {
+            if (pred(*it))
+                return it;
+        }
+
+        return last;
+    }
+}
+
+class GreaterThan
+{
+    double avg;
+public:
+    GreaterThan(double a) : avg{a} {
+    }
+
+    bool operator()(double value) const
+    {
+        return value > avg;
+    }
+};
+
+TEST_CASE("exercise")
+{
+    std::vector vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    double avg = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+    std::cout << "avg: " << avg << "\n";
+
+    GreaterThan gt_than_avg{avg};
+    REQUIRE(gt_than_avg(11));
+    REQUIRE(gt_than_avg(1) == false);
+
+    auto pos_gt_than_avg = std::find_if(vec.begin(), vec.end(), GreaterThan{avg});
+    std::cout << "Found: " << *pos_gt_than_avg << "\n";
+}
+
+TEST_CASE("std functors")
+{
+    std::greater<int> gt_int;
+
+    REQUIRE(gt_int(1, 2) == false);
+    REQUIRE(gt_int(2, 1));
+
+    std::vector vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    std::sort(vec.begin(), vec.end(), std::greater<int>{});
+    std::for_each(vec.begin(), vec.end(), Printer{});
+}
+
+////////////////////////////////////////////////////////
+// lambdy
+
+class Lambda_342763547623547
+{
+public:
+    auto operator()(int a) const
+    { 
+        std::cout << "Print from lambda: " << a << "\n"; 
+    }
+};
+
+namespace c_style
+{
+    int get_current_temp()
+    {
+        return 13;
+    }
+
+    void callback(void(*call_me)(int))
+    {
+        int temp = get_current_temp();
+        call_me(temp);
+    }
+}
+
+TEST_CASE("the simplest lambda")
+{
+    auto printer = [](int a){ std::cout << "Print from lambda: " << a << "\n"; };
+
+    printer(1);
+    printer(2);
+
+    SECTION("is interpreted as")
+    {
+        auto printer = Lambda_342763547623547{};
+    }
+
+    std::vector vec = {112, 212, 312};
+    vec = v2::calculate(vec, [](int a) { return a * 2; });
+    std::for_each(vec.begin(), vec.end(), printer);
+
+    std::vector<int*> vec_ptrs = { new int(12), new int(665), new int(1) };
+
+    std::sort(vec_ptrs.begin(), vec_ptrs.end(), [](int* pa, int* pb) { return *pa < *pb; });
+
+    std::for_each(vec_ptrs.begin(), vec_ptrs.end(), [](int* ptr) { std::cout << (*ptr) << " "; });
+    std::cout << "\n";
+
+    auto log = [](int value) { std::cout << "logging: " << value << "\n"; };
+    c_style::callback(log);
+}
+
+class Lambda_87364273548276345
+{
+    const double avg_;
+public:
+    Lambda_87364273548276345(double avg) : avg_{avg} {}
+    auto operator()(int x) const { return x > avg_; }
+};
+
+TEST_CASE("captures")
+{
+    std::vector vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    double avg = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+    std::cout << "avg: " << avg << "\n";
+
+    SECTION("capture by value")
+    {
+        auto pos_gt_than_avg = std::find_if(vec.begin(), vec.end(), [avg](int x) { return x > avg; });
+        REQUIRE(*pos_gt_than_avg == 6);
+    }
+
+    SECTION("capture by reference")
+    {
+        int sum = 0;
+        std::for_each(vec.begin(), vec.end(), [&sum](int x) { sum += x; });
+
+        REQUIRE(sum == 55);
+    }
+
+    SECTION("capture by value & reference")
+    {
+        int sum = 0;
+        int factor = 2;
+        std::for_each(vec.begin(), vec.end(), [&sum, factor](int x) { sum += factor * x; });
+
+        REQUIRE(sum == 110);
+    }
+}
+
+TEST_CASE("capture using = and &")
+{
+    int val1 = 1;
+    int val2 = 2;
+
+    auto l1 = [=] { return val1 * val2; };
+    auto l2 = [&] { ++val1; ++val2; };
+    auto l3 = [=, &val2] { ++val2; return val1 + 1; };
+    auto l4 = [&, val2] { ++val1; return val2 + 1; };
+}
+
+auto create_generator(int start)
+{
+    return [start]() mutable { return start++; };
+}
+
+TEST_CASE("generator")
+{
+    auto gen = create_generator(100);
+    REQUIRE(gen() == 100);
+    REQUIRE(gen() == 101);
+    REQUIRE(gen() == 102);
+
+    std::vector<int> vec(10);
+    std::generate_n(vec.begin(), 10, create_generator(13));
+    std::for_each_n(vec.begin(), 5, [](int n) { std::cout << n << " "; });
+    std::cout << "\n";
+}
+
+class Data
+{
+    const std::vector<int> data_;
+    double avg_;
+public:
+    Data(std::vector<int> data) : data_{std::move(data)}
+    {
+        avg_ = std::accumulate(data_.begin(), data_.end(), 0.0) / data_.size();
+    }
+
+    void print_gt_than_avg()
+    {
+        std::for_each(data_.begin(), data_.end(), [this](int x) { if (x > avg_) std::cout << x << " "; });
+        std::cout << "\n";
+    }
+};
+
+TEST_CASE("capturing this")
+{
+    Data data{{1, 2, 3, 5, 6, 7, 8, 9, 10}};
+    data.print_gt_than_avg();
 }
